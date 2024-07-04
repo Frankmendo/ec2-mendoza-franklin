c6f72390c02b3ca2829bbd69ab0feb3e3f23a8b1
frankmendo <frankmen1810@gmail.com>
2024-07-03 21:16:27 -0500

    modificacion en el mainactivity

--- app/src/main/java/com/modelo/ec2_mendoza_franklin/MainActivity.kt
+++ app/src/main/java/com/modelo/ec2_mendoza_franklin/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.modelo.ec2_mendoza_franklin.ui.theme.Ec2mendozafranklinTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +31,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ec2mendozafranklinTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // Configurar NavController para la navegación
+                val navController = rememberNavController()
+
+                // Definir NavHost con las rutas y destinos
+                NavHost(navController = navController, startDestination = "main_screen") {
+                    composable("main_screen") { MainScreen(navController) }
+                    composable("formulario_screen") { FormularioScreen(navController) }
+                    composable("listado_screen") { ListadoScreen(navController) }
                 }
             }
         }
@@ -31,17 +46,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen(navController: NavController) {
+    val context = LocalContext.current
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Ec2mendozafranklinTheme {
-        Greeting("Android")
-    }
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        content = { paddingValues ->
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.padding(paddingValues = paddingValues)
+                ) {
+                    Button(onClick = {
+                        navController.navigate("formulario_screen")
+                    }) {
+                        Text(text = "FORMULARIO")
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        navController.navigate("listado_screen")
+                    }) {
+                        Text(text = "LISTADO")
+                    }
+                }
+            }
+        }
+    )
 }
